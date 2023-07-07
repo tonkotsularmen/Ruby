9ec9cca2fcd23ba524bb5ecb164f1cfdaaebf057
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-07-07 08:57:49 +0000

    ZerWakaRuby[finish ch02]

--- greeting.rb
+++
@@ -1,5 +0,0 @@
-def greeting(name)
-  puts "hello #{name}"
-end
-
-greeting('John')
---
+++ zero2.rb
@@ -0,0 +1,78 @@
+puts 2 - 1
+puts 2 * 3
+puts 4 /  2
+puts 1 + 2 + 3
+puts 1 + 2 * 3
+puts (1 + 2) * 3
+puts 5 / 2
+puts 5.0 / 2.0
+puts 2 ** 8
+puts 7 % 3
+puts "こんにちは"
+puts "hello " + "world!"
+puts "こん" + "にち" + "は"
+puts 2 + 3
+puts "2" + "3"
+puts 3.to_s + "3"
+puts 3 + "3".to_i
+order = "カフェラテ"
+puts order
+puts "ご注文は" + "カフェラテ" + "ですね？"
+puts "カフェラテ" + "、オーダー入ります"
+order = "カフェラテ"
+puts "ご注文は" + order + "ですね？"
+puts order + "、オーダー入ります"
+puts "1 + 2の答えは#{1 + 2}です"
+order = "カフェラテ"
+puts "ご注文は" + order + "ですね？"
+puts "ご注文は#{order}ですね?"
+#puts "数字を入力してenterを押してください（2回）"
+#x = gets
+#y = gets
+#puts x + y
+#puts x.chomp + y
+#puts x.to_i + y.to_i
+CaffeLatte = "カフェラテ"
+puts CaffeLatte
+#CaffeLatte = "カフェラっテ"
+#puts CaffeLatte
+puts 1
+#BMIの計算
+puts 60.0 / (1.7 * 1.7)
+
+puts 1 + 3 # + 5
+puts 1
+=begin
+puts 2
+puts 3
+puts 4
+=end
+puts 5
+puts "irbを実行してみよう。"
+puts "コマンドプロンプトにirbと入力して実行すると起動できるよ"
+puts "exitで終了できるよ"
+#require "irb" Ruby2.5以降では省略できる
+a = 97463
+#binding.irb
+puts a
+c = 3.0 / 2.0
+#binding.irb
+d = c * 2
+puts d
+puts 2 + 3
+puts 2.0 * 2.0 * 3.14
+puts "Ruby"
+puts "abc" + "def"
+puts "123".to_i + "456".to_i
+cof = 400
+puts "コーヒー" + ":#{cof}円"
+esple = 100
+order = 2
+puts "合計:" + "#{cof + (esple * order)}円"
+total = 300 * 2
+tax = total * 0.08
+g = total + tax
+p g
+puts tax
+total = 300 * 2
+puts total
