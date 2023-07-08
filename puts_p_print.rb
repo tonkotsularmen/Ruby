c557f0653ed69abc3a9ad4cc5a0dde2d856b8b21
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-07-08 09:26:39 +0000

    zerowakaRuby[rest ch05]

--- puts_p_print.rb
+++
@@ -1,7 +0,0 @@
-drinks = ["水", "コーラ", "紅茶", "緑茶", "コーヒー", "プロテイン"]
-puts drinks
-puts "----------"
-p drinks
-puts "----------"
-print 123456
-print "みず"
---
+++ zero5.rb
@@ -0,0 +1,33 @@
+puts ["water", "cola", "tea"].size
+puts [1, 2, 3].sum
+puts [1, 2, 3].inject(:+)
+puts "平均算出"
+a = [1, 2, 3]
+puts a.sum / a.size
+puts "平均点算出"
+a = {算数: 60, 国語: 70, 社会: 90}
+puts a
+puts a.values.sum / a.size
+puts "平均を小数まで算出"
+a = [1, 1, 3]
+puts a.sum.to_f / a.size
+puts "============="
+a1 = [1, 1, 2]
+a2 = a1.uniq
+p a1
+p a2
+puts "============="
+a1 = [1, 1, 2]
+a2 = a1.uniq!
+p a1
+p a2
+puts "============="
+a1 = [1, 1, 2]
+a2 = a1.uniq
+p a1.object_id
+p a2.object_id
+puts "============="
+a1 = [1, 1, 2]
+a2 = a1.uniq!
+p a1.object_id
+p a2.object_id
