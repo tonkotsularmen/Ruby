cfe3bcd99918973b6e762d3df38f1b9c0586d80a
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-07-08 02:40:10 +0000

    zerowakaRuby[start ch03]

---
+++ zero3.rb
@@ -0,0 +1,20 @@
+puts 1 < 2
+puts 1 > 2
+puts 1 <= 2
+puts 1 >= 2
+wallet = 500
+puts wallet >= 300
+wallet = 100
+puts wallet >= 300
+puts 1 == 2
+puts 2 == 2
+puts 2 == 1 + 1
+puts 1 != 2
+puts 2 != 2
+puts "ruby" == "ruby"
+puts "ruby" != "ruby"
+puts "ruby" == "python"
+puts 2.even?
+puts 3.even?
+puts 2.odd?
+puts 3.odd?
