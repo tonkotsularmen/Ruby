b18ea08790243e247297b72403a78e75202fa17e
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-07-09 00:45:46 +0000

    zerowakaRuby[finish ch05]

--- zero5.rb
+++ zero5.rb
@@ -39,3 +39,71 @@ p [1, 3, 2, "2", "3"].each { |n| n.to_s}
 p [1, 3, 2, "2", "3"].map { |n| n.to_s}
 ["A", "B", "C", "c"].uniq {|n| n.downcase}
 p [1, 3, 2, "2", "3"].each 
+p [4, 7, 1].sample
+p [4, 7, 1].shuffle
+p [4, 7, 1].sort
+p ["hitomi", "achi", "tama"].sort
+p ["aya", "achi", "tama"].sort
+p ["aya", "achi", "Tama"].sort
+p [4, 7, 1].sort.reverse
+p "abcdef".reverse
+p "ラーメンチャーハン餃子セット".reverse
+order = ""
+["カフェラテ", "チーズケーキ"].each do |item|
+  order = order + item + "と"
+end 
+
+puts order
+puts ["カフェラテ"].join("と")
+puts ["カフェラテ", "チーズケーキ"].join("と")
+puts ["カフェラテ", "チーズケーキ", "バニラアイス"].join("と")
+
+p "カフェラテ チーズケーキ バニラアイス".split
+p "カフェラテとチーズケーキとバニラアイス".split("と")
+
+result = [1, 2, 3].map do |x|
+  x * 2
+end 
+p result
+
+result = [100, 200, 300].map do |x|
+  "#{x}円"
+end 
+p result
+
+result = ["abc", "123"].map do |text|
+  text.reverse
+end 
+p result
+
+result = ["abc", "123"].map{ |text| text.reverse}
+p result
+
+result = ["abc", "123"].map(&:reverse)
+p result
+
+# !練習問題!
+puts "5-1"
+p ["コーヒー", "カフェラテ"].size
+p [1, 2, 3, 4, 5].sum
+puts "5-2"
+p ["モカ", "カフェラテ", "モカ"].uniq
+p ["モカ", "カフェラテ", "モカ"].clear
+puts "5-3"
+p ["カフェラテ", "モカ", "カプチーノ"].sample
+puts ["大吉", "中吉", "末吉", "凶"].sample
+puts "5-4"
+p [100, 50, 300].sort
+p [100, 50, 300].sort.reverse
+puts "cba".reverse
+puts "5-5"
+p ["100", "50", "300"].join(",")
+p "100,50,300".split(",")
+p [1, 2, 3].map { |num| num * 2}
+p ["abc", "xyz"].map(&:reverse)
+
+names = ["aya", "achi", "Tama"].map do |name|
+  name.downcase
+end
+p names.sort
+p ["aya", "achi", "Tama"].map{|name| name.downcase}.sort
