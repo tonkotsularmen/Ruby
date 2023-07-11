471db4d89c7df8ca73070f07819a792ad7abb0cc
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-07-11 09:38:01 +0000

    zerowakaRuby[rest ch07]

--- try.rb
+++ try.rb
@@ -7,3 +7,14 @@ puts "----------"
 # 二次元配列やってみたい
 a = ["ラーメン"["醤油", "塩", "みそ", "とんこつ"], "チャーハン", "餃子"]
 puts a[0][0]
+
+puts "体重をkgで入力してください(例:80)"
+weight = gets
+puts "身長をmで入力してください(例：1.77)"
+height = gets
+
+def bmi(weight, height)
+  weight.to_f / (height.to_f * height.to_f)
+end
+
+puts "あなたのBMIは#{bmi(weight, height)}です"
--- zero7.rb
+++ zero7.rb
@@ -5,6 +5,64 @@ a = [1, 2, 3]
 sum = 0
 a.each do |x|
   sum += x
-end 
+end
 puts sum
 
+def area
+  2 * 2
+end
+
+result = area
+puts result
+
+def area(x)
+  x * x
+end
+
+puts area(2)
+puts area(3)
+
+def area2(x, y)
+  x * y 
+end 
+
+puts area2(2, 3)
+
+def thanks_and_receipt(receipt)
+  puts "ありがとうございました"
+  unless receipt
+    return
+  end
+  puts "こちら、レシートになります"
+end 
+
+thanks_and_receipt(false)
+thanks_and_receipt false
+
+puts "=========="
+def thanks_and_receipt(receipt)
+  greeting = "ありがとうございました。"
+  unless receipt
+    return greeting
+  end
+  greeting + "こちら、レシートになります"
+end 
+
+puts thanks_and_receipt(false)
+puts thanks_and_receipt(true)
+
+puts "=========="
+puts("こちら、レシートになります")
+puts "=========="
+
+puts thanks_and_receipt false
+puts "=========="
+
+def calc(num1, num2)
+  return "計算できません" if num1.zero? || num2.zero?
+  num1 * num2
+end 
+
+puts calc 4, 5
+puts calc 4, 0
+puts calc 0, 3
