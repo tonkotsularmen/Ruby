ee1dfe4179358ed1c55a5e012212b8019ee39c13
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-07-14 04:26:12 +0000

    インスタンス変数

--- zero8.rb
+++ zero8.rb
@@ -119,3 +119,21 @@ end
 drink = Drink.new#オブジェクト作成
 drink.order("カフェラテ")
 drink.reorder
+
+puts "======="
+class Drink
+  def order(item)
+    puts "#{item}をください"
+    @name = item
+  end 
+  def reorder
+    puts "#{@name}をもう一杯ください"
+  end 
+end 
+
+drink1 = Drink.new
+drink2 = Drink.new
+drink1.order("カフェラテ")
+drink2.order("モカ")
+drink1.reorder
+drink2.reorder
