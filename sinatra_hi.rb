c194c14a075ff379d801316298ced0039da083e2
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-07-23 08:42:36 +0000

    net/http,json

--- sinatra_hi.rb
+++ sinatra_hi.rb
@@ -1,4 +1,22 @@
-require "sinatra"
-get "/" do
-  "hi!"
-end 
+# require "sinatra"
+# get "/" do
+#   ["カフェラテ", "モカ", "コーヒー"].sample
+# end 
+
+# require "net/http"
+# require "uri"
+# uri = URI.parse("https://example.com/")
+# puts Net::HTTP.get(uri)
+
+
+# require "net/http"
+# require "uri"
+# require "json"
+# uri = URI.parse("https://igarashikuniaki.net/example.json")
+# result = Net::HTTP.get(uri)
+# hash = JSON.parse(result)
+# p hash
+# p hash["caffe latte"]
+
+require "json"
+p({mocha: 400}.to_json)
