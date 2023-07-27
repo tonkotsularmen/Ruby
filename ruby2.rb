e61e39412ab15ecec8914202e4836d659edbee06
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-07-27 12:33:55 +0000

    2.2.2

--- ruby2.rb
+++ ruby2.rb
@@ -1,4 +1,4 @@
-# 全てがオブジェクト
+# 2.2.1全てがオブジェクト
 p '1'.to_s
 p 1.to_s
 p nil.to_s
@@ -6,7 +6,7 @@ p true.to_s
 p false.to_s
 p /\d+/.to_s
 
-# メソッド呼び出し
+# 2.2.2メソッド呼び出し
 p 1.to_s()
 p 1.to_s
 p 10.to_s(16)
